821002dbac729018e8ef1b911e9938ba4ef9e0a6
Fernando Andrade <fernando.amdrade@gmail.com>
2021-02-24 23:54:53 -0300

    Move css do ExperienceBar para arquivo outro arquivo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 
 export default function Home() {
   return (
     <div className="container">
       <ExperienceBar />
+      <Countdown />
     </div>
   )
 }
